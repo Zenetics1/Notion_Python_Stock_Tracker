93efeb08575ea89fe054b427cc02d6e7e182a510
WiZK1 <spitfirexigames@gmail.com>
2025-12-19 20:38:50 -0500

    Adjusted program to query database for added stock symbols, then calling TwelveData API to update page rowwith new stock pricing/company name

--- main.py
+++ main.py
@@ -17,51 +17,82 @@ header = {
     "Content-Type": "application/json"
 }
 
-Notion_New_Page_Endpoint = "https://api.notion.com/v1/pages"
 Notion_DB_ID = "2cca3154e95980aa9118ff6cc25e4065"
+Notion_DB_URL = f"https://api.notion.com/v1/databases/{Notion_DB_ID}/query"
 
 """Initial Test for inputting stock symbol and retrieving price data from Twelve Data API"""
 td = TDClient(apikey=Twelve_Data_API_Key)
-stock_symbol = input("Pick a stock symbol: ")
-tst = td.price(symbol=stock_symbol).as_json()
 
-"""Payload Test with Twelve Data API price data"""
-payload = {
-    "parent":{
-        "database_id": Notion_DB_ID
-    },
-    "properties":{
-        "Name": {
-            "title": [
-                {
-                    "text": {
-                        "content": "Test Stock"
+response = requests.post(Notion_DB_URL, headers=header)
+
+DB_Query = response.json()
+
+for page in DB_Query['results']:
+    if page['properties']['Status']['status']['name'] == 'Add Stock':
+
+        page_id = page['id']
+
+        stock_symbol = page['properties']['Symbol']['rich_text'][0]['text']['content']
+        selected_stock = td.quote(symbol=stock_symbol).as_json()
+
+        selected_stock_name = selected_stock['name']
+        selected_stock_price_high = selected_stock['high']
+
+        Notion_Page_Endpoint = f"https://api.notion.com/v1/pages/{page_id}"
+
+        payload = {
+            "properties":{
+                "Symbol": {
+                    "rich_text": [
+                        {
+                            "text": {
+                                "content": stock_symbol
+                            }
+                        }
+                    ]
+                },
+                "Name": {
+                    "title": [
+                        {
+                            "text": {
+                                "content": selected_stock_name
+                            }
+                        }
+                    ]
+                },
+                "Price High":{
+                    "number": float(selected_stock_price_high)
+                },
+                "Status": {
+                    "status": {
+                        "name": "Continuously Update"
                     }
                 }
-            ]
-        },
-        "Ticker": {
-            "rich_text": [
-                {
-                    "text": {
-                        "content": stock_symbol
-                    }
-                }
-            ]
-        },
-        "Price":{
-            "number": float(tst['price'])
+            }
         }
-    }
-}
+        response = requests.patch(Notion_Page_Endpoint, headers=header, json=payload)
+        print(response.status_code)
+        print(response.json())
+    elif page['properties']['Status']['status']['name'] == "Continuously Update":
 
-response = requests.post(Notion_New_Page_Endpoint, headers=header, json=payload)
+        page_id = page['id']
 
-print(response.status_code)
-print(response.json())
+        stock_symbol = page['properties']['Symbol']['rich_text'][0]['text']['content']
+        
+        selected_stock = td.quote(symbol=stock_symbol).as_json()
+        selected_stock_price_high = selected_stock['high']
 
-page_id = response.json()
+        Notion_Page_Endpoint = f"https://api.notion.com/v1/pages/{page_id}"
 
+        payload = {
+            "properties":{
+                "Price High":{
+                    "number": float(selected_stock_price_high)
+                }
+            }
+        }
 
-
+        response = requests.patch(Notion_Page_Endpoint, headers=header, json=payload)
+        print(response.status_code)
+        print(response.json())
 
