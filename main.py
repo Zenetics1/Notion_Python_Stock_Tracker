d1e8c1e9663cc584329167f3059e84c366fa4b93
WiZK1 <spitfirexigames@gmail.com>
2025-12-22 20:57:29 -0500

    Added additional stock metrics, changing images based on stock performance, and additional formatting changes for large numbers

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import requests
 Twelve_Data_API_Key = os.getenv("TwelveData_API_Key")
 Notion_Secret = os.getenv("Notion_Internal_Secret")
 
-"""Notion API POST Request component setup"""
+"""Notion API POST Request header setup"""
 header = {
     "Authorization": f"Bearer {Notion_Secret}",
     "Notion-Version": "2022-06-28",
@@ -20,7 +20,7 @@ header = {
 Notion_DB_ID = "2cca3154e95980aa9118ff6cc25e4065"
 Notion_DB_URL = f"https://api.notion.com/v1/databases/{Notion_DB_ID}/query"
 
-"""Initial Test for inputting stock symbol and retrieving price data from Twelve Data API"""
+"""Querying stock symbol and retrieving price data from Twelve Data API"""
 td = TDClient(apikey=Twelve_Data_API_Key)
 
 response = requests.post(Notion_DB_URL, headers=header)
@@ -35,12 +35,31 @@ for page in DB_Query['results']:
         stock_symbol = page['properties']['Symbol']['rich_text'][0]['text']['content']
         selected_stock = td.quote(symbol=stock_symbol).as_json()
 
-        selected_stock_name = selected_stock['name']
-        selected_stock_price_high = selected_stock['high']
+
+        selected_name = selected_stock['name']
+
+        selected_price_current = td.price(symbol=stock_symbol).as_json()
+        selected_price_high = selected_stock['high']
+        selected_price_low = selected_stock['low']
+
+        selected_vol = selected_stock['volume']
+        selected_change = selected_stock['change']
 
         Notion_Page_Endpoint = f"https://api.notion.com/v1/pages/{page_id}"
 
+        """Changing icon next to stock name based on price change"""
+        if float(selected_change) > 0:
+            emoji_icon = "💹"
+        elif float(selected_change) == 0:
+            emoji_icon = "➖"
+        else:
+            emoji_icon = "📉"
+
         payload = {
+            "icon": {
+                "type": "emoji",
+                "emoji": emoji_icon
+            },
             "properties":{
                 "Symbol": {
                     "rich_text": [
@@ -55,13 +74,29 @@ for page in DB_Query['results']:
                     "title": [
                         {
                             "text": {
-                                "content": selected_stock_name
+                                "content": selected_name
                             }
                         }
-                    ]
+                    ],
+                },
+                "Price":{
+                    "number": float(selected_price_current['price'])
+                },
+                "Price Low":{
+                    "number": float(selected_price_low)
                 },
                 "Price High":{
-                    "number": float(selected_stock_price_high)
+                    "number": float(selected_price_high)
+                },
+                "Volume":{
+                    "rich_text": [{
+                        "text": {
+                            "content": f"{float(selected_vol):,}"
+                        }
+                    }]
+                },
+                "Change":{
+                    "number": float(selected_change)
                 },
                 "Status": {
                     "status": {
@@ -80,14 +115,48 @@ for page in DB_Query['results']:
         stock_symbol = page['properties']['Symbol']['rich_text'][0]['text']['content']
         
         selected_stock = td.quote(symbol=stock_symbol).as_json()
-        selected_stock_price_high = selected_stock['high']
+
+        selected_price_current = td.price(symbol=stock_symbol).as_json()
+        selected_price_high = selected_stock['high']
+        selected_price_low = selected_stock['low']
+
+        selected_vol = selected_stock['volume']
+        selected_change = selected_stock['change']
 
         Notion_Page_Endpoint = f"https://api.notion.com/v1/pages/{page_id}"
 
+        """Changing icon next to stock name based on price change"""
+        if float(selected_change) > 0:
+            emoji_icon = "💹"
+        elif float(selected_change) == 0:
+            emoji_icon = "➖"
+        else:
+            emoji_icon = "📉"
+
         payload = {
+            "icon":{
+                "type": "emoji",
+                "emoji": emoji_icon
+            },
             "properties":{
+                "Price":{
+                    "number": float(selected_price_current['price'])
+                },
+                "Price Low":{
+                    "number": float(selected_price_low)
+                },
                 "Price High":{
-                    "number": float(selected_stock_price_high)
+                    "number": float(selected_price_high)
+                },
+                "Volume":{
+                    "rich_text": [{
+                        "text": {
+                            "content": f"{float(selected_vol):,}"
+                        }
+                    }]
+                },
+                "Change":{
+                    "number": float(selected_change)
                 }
             }
         }
