ecf874e4e1faff1465ee2b9fd7a7661fe50d2258
WiZK1 <spitfirexigames@gmail.com>
2025-12-29 18:49:45 -0500

    Added Batch file for running script on Windows task scheduler

---
+++ run_script.bat
@@ -0,0 +1,9 @@
+@echo off
+
+REM Activate Virtual Environment
+call D:\Projects\Notion_Python_Stock_Tracker\.venv\Scripts\activate.bat
+
+REM Python Script
+python D:\Projects\Notion_Python_Stock_Tracker\main.py
+
+pause
